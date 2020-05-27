77fc7bcb48464fee08ccbdea384e5ffff4082440
MingquanFeng <850241532@sjtu.edu.cn>
2020-05-27 08:58:29 +0800

    change bash parameters

--- run_oag.sh
+++ run_oag.sh
@@ -1,10 +1,10 @@
 cd main
 
-CUDA_VISIBLE_DEVICES=3 python train.py \
+CUDA_VISIBLE_DEVICES=1 python train.py \
         -run_name oag \
         -data_root /home/fengmingquan/data/oag \
         -embedding_size 32 \
-        -n_layer 1 \
+        -n_layer 2 \
         -n_head 3 \
         -num_bs_instances 3 \
         -num_nested_calls 1 \
@@ -18,6 +18,6 @@ CUDA_VISIBLE_DEVICES=3 python train.py \
         -skip_trained \
         -no_recursion \
         -patience 20 \
-        -num_epochs 50 \
-        -num_batches_per_valid 2000 \
+        -num_epochs 3 \
+        -num_batches_per_valid 300 \
         -lr_decay_patience 900
